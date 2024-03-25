e773b4a5fb10182cc29a9ee719b8e9ddf14183e5
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-03-26 00:29:37 +0530

    signup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,47 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL:string;
+    JWT_SECRET:string
+  }
+}>()
 
-app.post('/api/v1/user/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signup', async(c) => {
+
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+try{
+  const user = await prisma.user.create({
+    data:{
+      username : body.username,
+      password : body.password,
+      name : body.name
+    }
+  })
+    const jwt = await sign({
+      id: user.id
+    },c.env.JWT_SECRET);
+
+    return c.text(jwt)
+} catch(e){
+  c.status(411);
+  return c.text('Invalid')
+}
+
 })
 
 app.post('/api/v1/user/signin', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
   return c.text('Hello Hono!')
 })
 
