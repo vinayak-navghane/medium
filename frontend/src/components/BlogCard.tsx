12d570469f6158550256b8c8455b4d7f4c10bd31
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-04-11 03:29:03 +0530

    added Blogs page & added BlogCard component

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,52 @@
+
+interface BlogCardProps {
+    authorName: string
+    title: string
+    content: string
+    publishedDate: string
+}
+
+export const BlogCard = ({
+    authorName,
+    title,
+    content,
+    publishedDate
+}: BlogCardProps) => {
+    return <div className=" p-4 border-b border-slate-200 pb-4">
+        <div className="flex">
+            <Avatar name={authorName} />
+            <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+                {authorName}
+            </div>
+            <div className="flex justify-center flex-col pl-2">
+                <Circle />
+            </div>
+            <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
+                {publishedDate}
+            </div>
+        </div>
+        <div className="text-xl font-semibold pt-2">
+            {title}
+        </div>
+        <div className="text-md font-thin">
+            {content.length > 100 ? content.slice(0, 100) + "..." : content}
+        </div>
+        <div className="text-slate-500 text-sm font-thin pt-4">
+            {`${Math.ceil(content.length / 100)} minute(s) read`}
+        </div>
+    </div>
+}
+
+function Circle() {
+    return <div className="h-1 w-1 rounded-full bg-slate-500">
+
+    </div>
+}
+
+function Avatar({ name }: { name: string }) {
+    return <div className="relative inline-flex items-center justify-center w-5 h-5 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+        <span className="text-xs font-extralight text-gray-600 dark:text-gray-300">
+            {name[0]}
+        </span>
+    </div>
+}
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,27 @@
+import { BlogCard } from "../components/BlogCard"
+
+
+export const Blogs = () => {
+    return <div className="flex justify-center">
+        <div className=" max-w-xl">
+            <BlogCard
+                authorName={"Vinayak"}
+                title={"How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                content={"How an ugly Single page website maked 5000 a month with affiliate marketing How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                publishedDate={"2nd Feb 2024"}
+            />
+            <BlogCard
+                authorName={"Vinayak"}
+                title={"How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                content={"How an ugly Single page website maked 5000 a month with affiliate marketing How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                publishedDate={"2nd Feb 2024"}
+            />
+            <BlogCard
+                authorName={"Vinayak"}
+                title={"How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                content={"How an ugly Single page website maked 5000 a month with affiliate marketing How an ugly Single page website maked 5000 a month with affiliate marketing"}
+                publishedDate={"2nd Feb 2024"}
+            />
+        </div>
+    </div>
+}
