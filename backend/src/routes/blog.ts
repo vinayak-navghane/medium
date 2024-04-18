490e5456bbe1c0b31819123b753a8e418a65ea67
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-04-18 19:24:57 +0530

    author id,name & few other properties from :/id endpoint

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -131,6 +131,16 @@ blogRouter.get('/:id', async(c) => {
         const blog = await prisma.blog.findFirst({
             where:{
                 id:Number(id)
+            },
+            select:{
+                id:true,
+                title:true,
+                content:true,
+                author:{
+                    select:{
+                        name:true
+                    }
+                }
             }
         })
     
