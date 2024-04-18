6a196e1c2dd561d0e60a58706bcdb5ebe0130b7a
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-04-18 19:24:11 +0530

    Revert "returned author name & few other properties from :/id endpoint"
    This reverts commit 4aecdc590133cc092feccd425bc43ae969a2c742.

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -131,15 +131,6 @@ blogRouter.get('/:id', async(c) => {
         const blog = await prisma.blog.findFirst({
             where:{
                 id:Number(id)
-            },
-            select:{
-                title:true,
-                content:true,
-                author:{
-                    select:{
-                        name:true
-                    }
-                }
             }
         })
     
