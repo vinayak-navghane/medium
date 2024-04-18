da1fcdcc9df45dd6de45335cfadfb15f0764bb35
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-04-18 19:02:17 +0530

    added useBlogs hook

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { BACKEND_URL } from "../config"
+
+
+interface Blog {
+    
+        "content": string
+        "title": string
+        "id": number
+        "author": {
+            "name": string
+        }
+    
+}
+
+export const useBlogs = () => {
+    const [loading, setLoading] = useState(true)
+    const [blogs, setBlogs] = useState<Blog[]>([])
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk`,{
+            headers:{
+                Authorization : localStorage.getItem("token")
+            }
+        })
+             .then(responce => {
+                setBlogs(responce.data.blogs)
+                setLoading(false)
+             })      
+
+    }, [])
+
+    return {
+        loading,
+        blogs
+    }
+}
