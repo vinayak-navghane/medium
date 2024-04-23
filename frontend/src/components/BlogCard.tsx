a4a58eb73034bcc2fb3120124ef5151b0e969d96
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-04-23 16:57:01 +0530

    added blogs skeleton component

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,58 +1,71 @@
-
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 interface BlogCardProps {
-    id : number,
-    authorName: string
-    title: string
-    content: string
-    publishedDate: string
+  id: number;
+  authorName: string;
+  title: string;
+  content: string;
+  publishedDate: string;
 }
 
 export const BlogCard = ({
-    id,
-    authorName,
-    title,
-    content,
-    publishedDate
+  id,
+  authorName,
+  title,
+  content,
+  publishedDate,
 }: BlogCardProps) => {
-    return <Link to={`/blog/${id}`}><div className=" p-4 border-b border-slate-200 pb-4 cursor-pointer">
+  return (
+    <Link to={`/blog/${id}`}>
+      <div className=" p-4 border-b border-slate-200 pb-4 cursor-pointer">
         <div className="flex">
-            <Avatar name={authorName} />
-            <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
-                {authorName}
-            </div>
-            <div className="flex justify-center flex-col pl-2">
-                <Circle />
-            </div>
-            <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
-                {publishedDate} 
-            </div>
+          <Avatar name={authorName} />
+          <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+            {authorName}
+          </div>
+          <div className="flex justify-center flex-col pl-2">
+            <Circle />
+          </div>
+          <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
+            {publishedDate}
+          </div>
         </div>
-        <div className="text-xl font-semibold pt-2">
-            {title}
-        </div>
+        <div className="text-xl font-semibold pt-2">{title}</div>
         <div className="text-md font-thin">
-            {content.length > 100 ? content.slice(0, 100) + "..." : content}
+          {content.length > 100 ? content.slice(0, 100) + "..." : content}
         </div>
         <div className="text-slate-500 text-sm font-thin pt-4">
-            {`${Math.ceil(content.length / 100)} minute(s) read`}
+          {`${Math.ceil(content.length / 100)} minute(s) read`}
         </div>
-    </div>
+      </div>
     </Link>
+  );
+};
+
+export function Circle() {
+  return <div className="h-1 w-1 rounded-full bg-slate-500"></div>;
 }
 
-function Circle() {
-    return <div className="h-1 w-1 rounded-full bg-slate-500">
-
+export function Avatar({
+  name,
+  size = "small",
+}: {
+  name: string;
+  size?: "small" | "big";
+}) {
+  return (
+    <div
+      className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full dark:bg-gray-600 ${
+        size === "small" ? "w-5 h-5" : "w-5 h-5"
+      }`}
+    >
+      <span
+        className={`${
+          size === "big" ? "text-lg" : "text-xs"
+        } font-extralight text-gray-300 dark:text-gray-300`}
+      >
+        {name[0]}
+      </span>
     </div>
-}
-
-export function Avatar({ name,size = "small" }: { name: string,
-     size?: "small" | "big" }) {
-    return <div className={`relative inline-flex items-center justify-center overflow-hidden bg-gray-600 rounded-full dark:bg-gray-600 ${size === "small" ? "w-5 h-5" : "w-5 h-5" }`}>
-        <span className={`${size === "big" ? "text-lg" : "text-xs"} font-extralight text-gray-300 dark:text-gray-300`}>
-            {name[0]}
-        </span>
-    </div>
+  );
 }
