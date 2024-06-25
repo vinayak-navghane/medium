c6d579f698c79261ccca69ea22445f62a08d2629
Vinayak Navghane <navghanevinayak24@gmail.com>
2024-06-25 19:00:05 +0530

    added " / " endpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Blogs />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/blog/:id" element={<Blog />} />
